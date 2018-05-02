b5da6fc3c27b553753c0f9aa50657f59a9b97cf3
sunchitsharma <sunchitsharma8@gmail.com>
2018-05-02 20:13:00 +0530

    +1

---
+++ workflows/wf_recheck.py
@@ -0,0 +1,12 @@
+from json_logic import jsonLogic
+import sys
+
+def main():
+	data = sys.argv[1]
+	# print 'Rechecking the conditions for Bp and Temp...'
+	# In case Doctor wants to add up something more when already called once
+	# print 'Get confirmation from Doctor for any further suggestion'
+	print 'Next Workflow to execute:'
+	print '_temp'
+if __name__ == '__main__':
+	main()
